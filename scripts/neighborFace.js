f9ab4f745b641f41ebf87d2976b671646580f336
Edbert Cheng <edbertcheng@gmail.com>
2022-08-08 12:49:45 -0400

    neighborFace

--- scripts/neighborFace.js
+++ scripts/neighborFace.js
@@ -21,4 +21,38 @@ complexity of your implementation.
  *Add a new object attribute to identify neighboring faces to each face object
 **/
 
-
+/*
+  Function that takes in the result of Algo #1 and a selected face
+  and returns a list of neighboring faces
+*/
+export function neighborFaces(input,faceName){
+  const index = input.findIndex(elem=>
+    elem.name == faceName
+  )
+  //faceName does not exist
+  if (index == -1){
+    return 'face does not exist in JSON'
+  }
+  //element to find adjacent faces
+  const faceElem = input.splice(index, 1) 
+  //find twinEdges to search for in other faces
+  const searchList = faceElem[0].edges.map(elem => {
+    const tStart = elem[1]
+    const tEnd = elem[0]
+    return [tStart, tEnd]
+  })
+  
+  //search through other faces
+  //i, j, k loop
+  const neighborFaces = []
+  input.forEach(face => {
+    face.edges.forEach(elem => {
+      searchList.forEach(tEdge => {
+        if (JSON.stringify(elem) == JSON.stringify(tEdge)){
+          neighborFaces.push(face.name)
+        }
+      })
+    })
+  })
+  return neighborFaces;
+}
